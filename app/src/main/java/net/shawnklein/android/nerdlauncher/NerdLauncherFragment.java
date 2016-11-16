250776d32ff1490bc8b141df0ea52acf2fe7fdd8
brechen <zappeln@gmail.com>
2016-11-15 19:58:47 -0600

    import activity_fragment and SingleFragmentActivity files

---
+++ app/src/main/java/net/shawnklein/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,13 @@
+package net.shawnklein.android.nerdlauncher;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+
+public class NerdLauncherFragment extends ListFragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
