1c2c3543e1db97673161813707f5e7bcca6b8e8d
brechen <zappeln@gmail.com>
2016-11-17 21:32:14 -0600

    NLFragment queries for intent activities'

--- app/src/main/java/net/shawnklein/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/net/shawnklein/android/nerdlauncher/NerdLauncherFragment.java
@@ -1,7 +1,13 @@
 package net.shawnklein.android.nerdlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
+
+import java.util.List;
 
 public class NerdLauncherFragment extends ListFragment {
     private static final String TAG = "NerdLauncherFragment";
@@ -9,5 +15,12 @@ public class NerdLauncherFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Log.i(TAG, "I've found " + activities.size() + " activities.");
     }
 }
