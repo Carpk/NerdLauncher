ed69c1f0d5010901cc6179a60ff2b65a99716226
brechen <zappeln@gmail.com>
2016-11-19 23:17:40 -0600

    displaying labels of activities in listView

--- app/src/main/java/net/shawnklein/android/nerdlauncher/NerdLauncherFragment.java
+++ app/src/main/java/net/shawnklein/android/nerdlauncher/NerdLauncherFragment.java
@@ -6,7 +6,13 @@ import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class NerdLauncherFragment extends ListFragment {
@@ -22,5 +28,28 @@ public class NerdLauncherFragment extends ListFragment {
         PackageManager pm = getActivity().getPackageManager();
         List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
         Log.i(TAG, "I've found " + activities.size() + " activities.");
+
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo resolveInfo, ResolveInfo t1) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        resolveInfo.loadLabel(pm).toString(),
+                        t1.loadLabel(pm).toString());
+
+            }
+        });
+
+        ArrayAdapter<ResolveInfo> adapter = new ArrayAdapter<ResolveInfo>(
+                getActivity(), android.R.layout.simple_list_item_1, activities) {
+            public View getView(int pos, View convertView, ViewGroup parent) {
+                PackageManager pm = getActivity().getPackageManager();
+                View v = super.getView(pos, convertView, parent);
+                TextView tv = (TextView) v;
+                ResolveInfo ri = getItem(pos);
+                tv.setText(ri.loadLabel(pm));
+                return v;
+            }
+        };
     }
 }
